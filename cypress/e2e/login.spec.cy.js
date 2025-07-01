487aa6cb49719e30db2ac8a25f1236a1a3306a28
Rafael Bortoluzzi <rafaeldnbr@hotmail.com>
2025-07-01 09:25:51 -0300

    Base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -10,9 +10,7 @@ describe("Orange HRM Tests", () => {
   };
 
   it("Login - Success", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username);
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password);
     cy.get(selectorsList.loginButton).click();
@@ -20,9 +18,7 @@ describe("Orange HRM Tests", () => {
     cy.get(selectorsList.topbarHeader).contains("Dashboard");
   });
   it("Login - Failure", () => {
-    cy.visit(
-      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-    );
+    cy.visit("/auth/login");
     cy.get(selectorsList.usernameField).type(userData.userFail.username);
     cy.get(selectorsList.passwordField).type(userData.userFail.password);
     cy.get(selectorsList.loginButton).click();
