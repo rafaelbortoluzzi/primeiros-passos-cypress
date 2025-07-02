fcf24670e58527861ee116859a5cc928fe4baba7
Rafael Bortoluzzi <rafaeldnbr@hotmail.com>
2025-07-02 17:28:39 -0300

    Added random data through chanceJs

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,10 +4,19 @@ import DashboardPage from "../pages/dashboardPage";
 import MenuPage from "../pages/menuPage";
 import MyInfoPage from "../pages/myInfoPage";
 
+const Chance = require("chance");
+
+const chance = new Chance();
 const loginPage = new LoginPage();
 const dashboardPage = new DashboardPage();
 const menuPage = new MenuPage();
 const myInfoPage = new MyInfoPage();
+
+function generateAnoDiaMes() {
+  const dateStr = chance.date({ string: true, american: false });
+  const [day, month, year] = dateStr.split("/");
+  return `${year}-${month}-${day}`;
+}
 
 describe("Orange HRM Tests", () => {
   it("User Info Update - Success", () => {
@@ -18,8 +27,19 @@ describe("Orange HRM Tests", () => {
     );
     dashboardPage.checkIfDashboardPage();
     menuPage.accessMyInfoPage();
-    myInfoPage.fillPersonalDetails("Rafael", "Danilo", "Bortoluzzi");
-    myInfoPage.fillEmployeeDetails("Employee", "12312", "111222", "2025-01-01");
+    myInfoPage.fillPersonalDetails(
+      chance.first(),
+      chance.string(),
+      chance.last()
+    );
+    myInfoPage.fillEmployeeDetails(
+      "Employee",
+      chance.integer({ min: 1000, max: 9999 }),
+      chance.integer({ min: 100000, max: 999999 }),
+      generateAnoDiaMes(),
+      chance.integer({ min: 2, max: 120 }),
+      chance.integer({ min: 2, max: 4 })
+    );
     myInfoPage.submitData();
   });
 });
--- cypress/pages/myinfoPage.js
+++ cypress/pages/myinfoPage.js
@@ -14,15 +14,28 @@ class MyInfoPage {
       dateCloseButton: ".--close",
       submitButton: ".oxd-button--medium",
       genderButton: ".oxd-radio-input",
+      comboboxField: ".oxd-select-text",
+      firstItemCombobox: ".oxd-select-dropdown > :nth-child(19)",
+      secondItemCombobox: ".oxd-select-dropdown > :nth-child(3)",
     };
     return selectors;
+  }
+  getDropdownItemSelector(index) {
+    return `.oxd-select-dropdown > :nth-child(${index})`;
   }
   fillPersonalDetails(firstName, middleName, lastName) {
     cy.get(this.selectorsList().firstNameField).clear().type(firstName);
     cy.get(this.selectorsList().middleNameField).clear().type(middleName);
     cy.get(this.selectorsList().lastNameField).clear().type(lastName);
   }
-  fillEmployeeDetails(employeeId, OtherId, driversLicense, dateOfBirth) {
+  fillEmployeeDetails(
+    employeeId,
+    OtherId,
+    driversLicense,
+    dateOfBirth,
+    nationality,
+    maritalStatus
+  ) {
     cy.get(this.selectorsList().genericField).eq(3).clear().type(employeeId);
     cy.get(this.selectorsList().genericField).eq(4).clear().type(OtherId);
     cy.get(this.selectorsList().genericField)
@@ -31,6 +44,10 @@ class MyInfoPage {
       .type(driversLicense);
     cy.get(this.selectorsList().genericField).eq(6).clear().type(dateOfBirth);
     cy.get(this.selectorsList().dateCloseButton).click();
+    cy.get(this.selectorsList().comboboxField).eq(0).click();
+    cy.get(this.getDropdownItemSelector(nationality)).click();
+    cy.get(this.selectorsList().comboboxField).eq(1).click();
+    cy.get(this.getDropdownItemSelector(maritalStatus)).click();
     cy.get(this.selectorsList().genderButton).eq(1).click();
   }
   submitData() {
